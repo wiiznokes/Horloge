a45531adf2820434e8c983e2245ff3463aa66a59
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2022-01-24 21:15:05 +0100

    avancé sur le choix de l'horaire de l'alarme

--- app/src/main/java/fr/wiiznokes/horloge11/app/AddActivity.java
+++ app/src/main/java/fr/wiiznokes/horloge11/app/AddActivity.java
@@ -2,9 +2,12 @@ package fr.wiiznokes.horloge11.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.Space;
 
 import fr.wiiznokes.horloge11.utils.*;
@@ -24,7 +27,13 @@ public class AddActivity extends AppCompatActivity {
 
     private final String fileName = "save.txt";
 
+    private String[] numberList = {"3","4","5","6","7","8","9"};
+
     private EditText alarmName;
+    private EditText alarmHours;
+    private RadioButton radioMonday;
+
+
 
 
 
@@ -34,12 +43,82 @@ public class AddActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
-
+        //lecture et recuperation du dernier objet Alarm du fichier
         List<Object> Array1 = read(fileName);
         Alarm AlarmNew = (Alarm) Array1.get(Array1.size()-1);
 
+        //nom de l'alarme
         this.alarmName = findViewById(R.id.textView12);
         alarmName.setText(AlarmNew.getNameAlarm());
+
+        //heure de l'alarme
+
+        this.alarmHours = findViewById(R.id.textView13);
+        this.radioMonday = findViewById(R.id.radioButton);
+
+
+        alarmHours.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String alarmHoursTxt = alarmHours.getText().toString();
+                //remise a zero si taille depasse 5
+                if (alarmHoursTxt.length() > 5) {
+                    alarmHours.setText(":");
+                }
+
+                //remplacement de la premiere case par 0 quand elle est pas égale a 1 ou 2 et quand il y a au moins un caractère avant ":"
+                if (alarmHoursTxt.indexOf(":") > 0) {
+                    //verif
+                    for (String number:numberList) {
+                        if (alarmHoursTxt.substring(0, 1).contains(number)) {
+                            //
+                            alarmHours.setText("0" + alarmHoursTxt.substring(0, 1) + ":" + alarmHoursTxt.substring(alarmHoursTxt.indexOf(":") + 1, alarmHoursTxt.length()));
+
+                            //mettre la selection à la fin
+                            alarmHoursTxt = alarmHours.getText().toString();
+                            alarmHours.setSelection(alarmHoursTxt.length());
+                            break;
+                        }
+                    }
+                }
+                //si suppression de ":"
+                if (!(alarmHoursTxt.contains(":"))){
+                    alarmHours.setText(":");
+                    alarmHours.setSelection(0);
+                }
+                //heure superieur à 23
+                if (Integer.parseInt(alarmHoursTxt.substring(0, alarmHoursTxt.indexOf(":"))) > 23){
+                    alarmHours.setText("2:"+alarmHoursTxt.substring(alarmHoursTxt.indexOf(":")+1, alarmHoursTxt.length()));
+                    //mettre selection avant :
+                    alarmHoursTxt = alarmHours.getText().toString();
+                    alarmHours.setSelection(alarmHoursTxt.indexOf(":"));
+                }
+                //minute
+                //chiffre des dizaine > 5
+                if (Integer.parseInt(alarmHoursTxt.substring(alarmHoursTxt.indexOf(":"), alarmHoursTxt.indexOf(":")+1)) > 5){
+                    alarmHours.setText(alarmHoursTxt.substring(0, alarmHoursTxt.indexOf(":")-1) + ":");
+                    //mettre selection avant ":"
+                    alarmHoursTxt = alarmHours.getText().toString();
+                    alarmHours.setSelection(alarmHoursTxt.indexOf(":"));
+                }
+
+            }
+        });
+
+
+
+
 
 
     }
